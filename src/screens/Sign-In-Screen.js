3392c931b76c383c4da1ee34a919b88161da8c10
matiassamuel <128171656+matiassamuelriera@users.noreply.github.com>
2024-10-08 12:55:09 -0300

    preyecto empezado en la peligrosa

---
+++ src/screens/Sign-In-Screen.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { AppProvider, SignInPage } from '@toolpad/core';
+import { useTheme } from '@mui/material/styles';
+// preview-start
+const providers = [
+
+  { id: 'google', name: 'Google' },
+
+];
+
+// preview-end
+
+const signIn = async (provider) => {
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`Sign in with ${provider.id}`);
+      resolve();
+    }, 500);
+  });
+  return promise;
+};
+
+export default function OAuthSignInPage() {
+  const theme = useTheme();
+  return (
+    // preview-start
+    <AppProvider theme={theme}>
+      <SignInPage signIn={signIn} providers={providers} />
+    </AppProvider>
+    // preview-end
+  );
+}
